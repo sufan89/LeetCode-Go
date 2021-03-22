90ca49272d7e3d47b1f388711f78f5ff2aff3218
杨贵超 <yangguichao@xsyxsc.com>
2021-03-22 18:37:43 +0800

    add node project

---
+++ leetcode/0001.Two-Sum/towSum.js
@@ -0,0 +1,9 @@
+/**
+ * @param   {number[]} nums
+ * @param {number} target
+ * @returns {number[]}
+ */
+var towSum = function (nums, target) {
+  return [0, 0];
+};
+module.exports = towSum;
---
+++ leetcode/0001.Two-Sum/twoSumTest.js
@@ -0,0 +1,3 @@
+// import { towSum } from "./towSum";
+var towSum = require("./towSum");
+console.log(towSum([3, 2, 4], 6));
