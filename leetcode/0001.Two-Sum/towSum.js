6254baaabbe77181d678db9d6c1b19b205faeedf
杨贵超 <yangguichao@xsyxsc.com>
2021-03-23 11:15:32 +0800

    0001

--- leetcode/0001.Two-Sum/towSum.js
+++ leetcode/0001.Two-Sum/towSum.js
@@ -4,6 +4,15 @@
  * @returns {number[]}
  */
 var towSum = function (nums, target) {
-  return [0, 0];
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let cha = target - nums[i];
+    if (map.has(cha)) {
+      return [map.get(cha), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  //   return [0, 0];
 };
 module.exports = towSum;
--- leetcode/0001.Two-Sum/twoSumTest.js
+++ leetcode/0001.Two-Sum/twoSumTest.js
@@ -1,3 +1,4 @@
 // import { towSum } from "./towSum";
 var towSum = require("./towSum");
 console.log(towSum([3, 2, 4], 6));
+console.log(towSum([0, 8, 7, 3, 3, 4, 2], 11));
